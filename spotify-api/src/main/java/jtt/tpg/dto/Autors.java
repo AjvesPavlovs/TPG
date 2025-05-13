5279a1773219510217d0db264e42181dfa380371
lietotajs <lietotajs@224-14>
2025-05-13 10:31:22 +0300

    daudz fignas

--- spotify-api/src/main/java/jtt/tpg/dto/Autors.java
+++ spotify-api/src/main/java/jtt/tpg/dto/Autors.java
@@ -4,13 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "autors")
 public class Autors {
-	@Id
 	@GeneratedValue
 	private int id;
+	@NotNull
 	private String name;
 	
 	public Autors() {}
