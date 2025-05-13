b6fd8faccf55dd2d402ed9bb4f38de6ce83ab912
lietotajs <lietotajs@224-14>
2025-05-13 13:09:41 +0300

    sql

---
+++ spotify-api/spotifyapi.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS spotifyapi CHARACTER SET utf8mb4 COLLATE utf8mb4_latvian_ci;
+
+CREATE TABLE IF NOT EXISTS spotifyapi.authors(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name  VARCHAR(255) NOT NULL,
+	followers INT NOT NULL,
+	genre VARCHAR(255) NOT NULL,
+	popularity INT NOT NULL);
