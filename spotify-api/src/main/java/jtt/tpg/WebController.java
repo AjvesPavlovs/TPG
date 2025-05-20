f3e6dbd4a236b6d3b4cbdf1807a1061d7861e609
lietotajs <lietotajs@224-14>
2025-05-20 10:13:50 +0300

    asd

--- spotify-api/src/main/java/jtt/tpg/WebController.java
+++ spotify-api/src/main/java/jtt/tpg/WebController.java
@@ -4,17 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import jtt.tpg.dao.AutorDAO;
+
+import jtt.tpg.dao.ArtistDAO;
+
 
 @Controller
 public class WebController {
 	
 	@Autowired
-	AutorDAO autorDAO;
+	ArtistDAO artistDAO;
 	
     @GetMapping("/")
     public String home(Model model) {
-    	String autorName = autorDAO.getByID(1).getName();
+    	String autorName = artistDAO.getByID(1).getName();
         model.addAttribute("message", autorName);
         return "index"; // Maps to index.html in templates folder
     }
